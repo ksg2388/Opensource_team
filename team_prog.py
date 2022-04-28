0ef3ed5ae56d49c9e4ad5086da11ba7afd147d73
ksg2388 <45422827+ksg2388@users.noreply.github.com>
2022-04-28 21:52:39 +0900

    Update team_prog.py

--- team_prog.py
+++ team_prog.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 from modul2 import *
 from fibonacci_number import *
+from union_intersection import *
 
 if __name__ == '__main__':
     while True:
@@ -8,10 +9,11 @@ if __name__ == '__main__':
         if (n == 1):
             num = input('input bin number:')
             binTohex(num)
+        elif (n == 2):
+            Arr()
         elif (n == 3):
             fibo = int(input('fibonacci number:'))
             fibonacci_number(fibo)
         elif (n == 4):
             print('exit the program...')
             break
-    
