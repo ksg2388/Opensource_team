a51f13b910e00258bb48e37f2c9a21b42f04011b
sxngjxn <96710588+sxngjxn@users.noreply.github.com>
2022-04-28 16:26:15 +0900

    Add files via upload

--- union:intersection.py
+++ union:intersection.py
@@ -1,21 +1,19 @@
-#union/intersection
-arr1 = list(map(int, input().split()))
-arr2 = list(map(int, input().split()))
+def Arr():
+   arr1 = list(map(int, input().split()))
+   print("input the 1st list :", arr1)
+   arr2 = list(map(int, input().split()))
+   print("input the 2st list :", arr2)
 
-result = [] # 중복 제거된 값들이 들어갈 리스트
+def union(arr1, arr2):
+   for value in arr2:
+      if value not in arr1:
+         arr1.append(value)
+   arr1.sort()
+   print("union : ", arr1)
 
-print("input the 1st list :", arr1)
-print("input the 2nd list :", arr2)
-
-for value in arr2:
-   if value not in arr1:
-      arr1.append(value)
-arr1.sort()
-
-print("union : ",arr1)
-
-for value in arr1:
-   if value in arr2:
-      result.append(value)
-
-print("intersection : ", result)
+def intersection(arr1, arr2, result):
+   for value in arr1:
+      if value in arr2:
+         result.append(value)
+   result.sort()
+   print("intersection : ", result)
